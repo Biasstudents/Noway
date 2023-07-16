cfbd7a3e0ae3b42eed4eb4031a2ba3b86a759f81
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-07-17 00:55:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -12,20 +12,16 @@ async def stress_test(url, num_requests, session):
 
 async def main(num_requests):
     url = input("Enter the website URL (including http or https): ")
+    duration = int(input("Enter the duration of the stress test in seconds: "))
     connector = aiohttp.TCPConnector(limit=0)
     timeout = aiohttp.ClientTimeout(total=0)
     async with aiohttp.ClientSession(connector=connector, timeout=timeout) as session:
-        while True:
-            try:
-                response = await session.get(url)
-                response.raise_for_status()
-                print(f"Website is up, sending {num_requests} requests")
-                await stress_test(url, num_requests, session)
-            except (aiohttp.ClientError, aiohttp.ClientResponseError):
-                print(f"Website is down, sending {num_requests // 2} requests")
-                await stress_test(url, num_requests // 2, session)
+        start_time = asyncio.get_event_loop().time()
+        while asyncio.get_event_loop().time() - start_time < duration:
+            print(f"Sending {num_requests} requests")
+            await stress_test(url, num_requests, session)
             await asyncio.sleep(1)
 
 if __name__ == '__main__':
-    num_requests = 1000
+    num_requests = 10000
     asyncio.run(main(num_requests))
