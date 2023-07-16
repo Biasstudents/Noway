e0bf09461f6397e1fdd5e041c98dfcf901fa3621
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-07-17 00:39:37 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import aiohttp
 import asyncio
 
-async def stress_test(url, num_requests):
+async def stress_test(url, num_requests, session):
+    tasks = []
+    for i in range(num_requests):
+        if i % 2 == 0:
+            tasks.append(session.get(url))
+        else:
+            tasks.append(session.head(url))
+    await asyncio.gather(*tasks)
+
+async def main(num_requests):
+    url = input("Enter the website URL (including http or https): ")
     connector = aiohttp.TCPConnector(limit=0)
     timeout = aiohttp.ClientTimeout(total=0)
     async with aiohttp.ClientSession(connector=connector, timeout=timeout) as session:
-        tasks = []
-        for i in range(num_requests):
-            if i % 2 == 0:
-                tasks.append(session.get(url))
-            else:
-                tasks.append(session.head(url))
-        await asyncio.gather(*tasks)
+        while True:
+            try:
+                response = await session.get(url)
+                response.raise_for_status()
+                print(f"Website is up, sending {num_requests} requests")
+                await stress_test(url, num_requests, session)
+            except (aiohttp.ClientError, aiohttp.ClientResponseError):
+                print(f"Website is down, sending {num_requests // 2} requests")
+                await stress_test(url, num_requests // 2, session)
+            await asyncio.sleep(1)
 
 if __name__ == '__main__':
-    url = 'https://www.example.com'
-    num_requests = 10000
-    asyncio.run(stress_test(url, num_requests))
+    num_requests = 1000
+    asyncio.run(main(num_requests))
