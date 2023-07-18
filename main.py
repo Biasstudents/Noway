31dd800f059a5eed0de97d62679eaaa3ab71bf62
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-07-18 16:01:05 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-import requests
+import aiohttp
+import asyncio
 import threading
 
-url = 'https://tls.mrrage.xyz/nginx_status'
+# Set the number of threads to use
 num_threads = 1000
 
-def stress_test():
-    while True:
-        response = requests.get(url)
-        print(response.status_code)
+# Set the URL of the website to test
+url = "https://tls.mrrage.xyz/nginx_status"
 
+# Create a TCPConnector with an unlimited connection pool
+connector = aiohttp.TCPConnector(limit=0)
+
+async def stress_test():
+    async with aiohttp.ClientSession(connector=connector) as session:
+        while True:
+            await send_request(session)
+
+async def send_request(session):
+    async with session.get(url) as response:
+        print(f"Status code: {response.status}")
+
+def run_stress_test():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(stress_test())
+
+# Create and start the threads
 threads = []
 for i in range(num_threads):
-    t = threading.Thread(target=stress_test)
-    t.start()
-    threads.append(t)
+    thread = threading.Thread(target=run_stress_test)
+    thread.start()
+    threads.append(thread)
 
-for t in threads:
-    t.join()
+# Wait for all threads to finish
+for thread in threads:
+    thread.join()
