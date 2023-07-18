23a6c8188facf6a948c08434110e1afb78b910d0
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-07-18 17:14:28 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import threading
 import time
 from termcolor import colored
 
-# Set the number of threads to use
-num_threads = int(input("Enter the number of threads to use: "))
-
 # Set the URL of the website to test
 url = input("Enter the website URL (including http or https): ")
+
+# Set the number of threads to use
+num_threads = int(input("Enter the number of threads to use: "))
 
 # Set the duration of the stress test in seconds
 duration = int(input("Enter the duration of the stress test in seconds: "))
@@ -18,21 +18,24 @@ status_interval = 10
 def stress_test():
     scraper = cloudscraper.create_scraper()
     start_time = time.time()
-    print(colored(f"Stress test started on {url} successfully!", "blue"))
+    print(colored(f"Stressing started on {url} successfully!", "yellow"))
     while time.time() - start_time < duration:
         send_request(scraper)
         time.sleep(status_interval)
         check_status(scraper)
-    print(colored("Stress test ended.", "blue"))
+    print(colored("Stress test ended.", "yellow"))
 
 def send_request(scraper):
     response = scraper.get(url)
 
 def check_status(scraper):
     try:
+        start_time = time.time()
         response = scraper.get(url)
+        end_time = time.time()
+        response_time = end_time - start_time
         if response.status_code == 200:
-            print(colored(f"{url} is online", "green"))
+            print(colored(f"{url} is online (response time: {response_time:.2f} seconds)", "green"))
         else:
             print(colored(f"{url} is offline", "red"))
     except:
