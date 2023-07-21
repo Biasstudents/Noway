33b038718f1024e99a34e47c74c716f653b29841
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-07-21 22:05:17 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import cloudscraper
+import asyncio
 import httpx
 import socket
-import multiprocessing
+import threading
 import time
 from colorama import Fore, Style, init
 
@@ -17,8 +17,8 @@ elif protocol in ["tcp", "udp"]:
     ip = input("Enter the IP address of the server: ")
     port = int(input("Enter the port number: "))
 
-num_processes = int(input("Enter the number of processes to use: "))
-num_connections = int(input("Enter the number of connections to use: "))
+num_threads = int(input("Enter the number of threads to use: "))
+
 duration = int(input("Enter the duration of the stress test in seconds: "))
 
 if protocol == "udp":
@@ -27,16 +27,15 @@ elif protocol == "tcp":
     packet_size = 65535 # Maximum size for a TCP packet
 
 packet_counter = 0
-packet_counter_lock = multiprocessing.Lock()
+packet_counter_lock = threading.Lock()
 
-def stress_test():
+async def stress_test(thread_id):
     global packet_counter
 
     if protocol == "cfb":
         client = cloudscraper.create_scraper()
     elif protocol in ["get", "head"]:
-        limits = httpx.Limits(max_connections=num_connections, max_keepalive_connections=num_connections)
-        client = httpx.Client(http2=True, limits=limits)
+        client = httpx.AsyncClient(pool_limits=httpx.PoolLimits(max_connections=num_threads))
     elif protocol in ["tcp", "udp"]:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     else:
@@ -45,26 +44,26 @@ def stress_test():
 
     start_time = time.time()
     last_print_time = start_time - 9
-    if multiprocessing.current_process().name == 'MainProcess':
+    if thread_id == 0:
         if protocol in ["get", "head", "cfb"]:
             print(Fore.YELLOW + f"Stress testing started on {url} successfully!" + Style.RESET_ALL)
         elif protocol in ["tcp", "udp"]:
             print(Fore.YELLOW + f"Stress testing started on {ip}:{port} successfully!" + Style.RESET_ALL)
     while time.time() - start_time < duration:
         if protocol in ["get", "head", "cfb"]:
-            send_request(client)
+            await send_request(client)
             with packet_counter_lock:
                 packet_counter += 1
         elif protocol in ["tcp", "udp"]:
             send_packet(sock)
             with packet_counter_lock:
                 packet_counter += 1
-        if multiprocessing.current_process().name == 'MainProcess' and time.time() - last_print_time >= 10:
+        if thread_id == 0 and time.time() - last_print_time >= 10:
             with packet_counter_lock:
                 if protocol in ["get", "head", "cfb"]:
                     try:
                         response_start_time = time.time()
-                        response = httpx.get(url)
+                        response = await client.get(url)
                         response_end_time = time.time()
                         response_time = round((response_end_time - response_start_time) * 1000, 2)
                         print(Fore.GREEN + f"Website is up. Response time: {response_time} ms." + Style.RESET_ALL)
@@ -74,12 +73,12 @@ def stress_test():
                     print(f"Sent {packet_counter} packets to {ip}:{port}")
                 last_print_time = time.time()
 
-def send_request(client):
+async def send_request(client):
     try:
         if protocol == "get":
-            response = client.get(url)
+            response = await client.get(url)
         elif protocol == "head":
-            response = client.head(url)
+            response = await client.head(url)
     except Exception as e:
         pass # Suppress error messages
 
@@ -94,13 +93,15 @@ def send_packet(sock):
     except Exception as e:
         pass 
 
-processes = []
-for i in range(num_processes):
-    process = multiprocessing.Process(target=stress_test)
-    process.start()
-    processes.append(process)
+async def main():
+    tasks = []
+    for i in range(num_threads):
+        task = asyncio.ensure_future(stress_test(i))
+        tasks.append(task)
 
-for process in processes:
-    process.join()
+    await asyncio.gather(*tasks)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
 
 print(Fore.YELLOW + "Stress test ended." + Style.RESET_ALL)
